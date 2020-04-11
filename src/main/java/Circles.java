5137731a8eb177bc5f18fdb2dca2cb6523ae6bbe
zdenek-nemec <zdenek.nemec@artin.cz>
2020-04-11 13:35:54 +0200

    Sample unit test added

--- src/main/java/Circles.java
+++ src/main/java/Circles.java
@@ -1,10 +1,10 @@
 public class Circles {
-    public static double get_circle_area(double radius) {
+    public static double getCircleArea(double radius) {
         return Math.PI * Math.pow(radius, 2);
     }
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Circle area is " + get_circle_area(42));
+        System.out.println(getCircleArea(42));
     }
 }
